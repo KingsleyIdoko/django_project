4d789c54fa82bd673cdd89abfd81b528042cb4f9
kingsley <idoko.omokhodion@gmail.com>
2024-08-01 22:46:48 +0200

    testing driven development

--- django_project/videos/models.py
+++ django_project/videos/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 # Create your models here.
 
 class Video(models.Model):
@@ -7,12 +8,14 @@ class Video(models.Model):
         PUBLISH = 'PU','Publish'
         DRAFT = 'DR','DRAFT'
 
-    title = models.CharField(max_length=50)
-    description = models.TextField(blank=True, null=True)
-    slug = models.SlugField(blank=True, null=True)
-    video_id = models.CharField(max_length=50)
-    active = models.BooleanField(default=True)
-    state = models.CharField(max_length=2, choices=VideoStateOptions.choices,default=VideoStateOptions.DRAFT)
+    title               = models.CharField(max_length=50)
+    description         = models.TextField(blank=True, null=True)
+    slug                = models.SlugField(blank=True, null=True)
+    video_id            = models.CharField(max_length=50, unique=True)
+    active              = models.BooleanField(default=True)
+    timestamp           = models.DateTimeField(auto_now_add=True)
+    updated             = models.DateTimeField(auto_now=True)
+    state               = models.CharField(max_length=2, choices=VideoStateOptions.choices,default=VideoStateOptions.DRAFT)
     published_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
 
     @property
@@ -27,6 +30,8 @@ class Video(models.Model):
             self.published_timestamp = timezone.now()
         elif self.state == self.VideoStateOptions.DRAFT:
             self.published_timestamp = None
+        if not self.slug:
+            self.slug = slugify(self.title)
         super().save(*args,**kwargs)
 
 class VideoAllProxy(Video):
--- django_project/videos/tests.py
+++ django_project/videos/tests.py
@@ -1,22 +1,34 @@
 from django.test import TestCase
-
+from django.utils import timezone
+from django.utils.text import slugify
 # Create your tests here.
 from .models import Video
 
 class VideoModelTestCase(TestCase):
 
     def setUp(self):
-        Video.objects.create(title="This is my first title")
+        self.obj_a  = Video.objects.create(title="This is my first title", video_id='abc')
+        self.obj_b  = Video.objects.create(title="This is my 2nd title", state=Video.VideoStateOptions.PUBLISH,video_id='acc')
 
+    def test_slugify(self):
+        title = self.obj_a.title
+        test_slug = slugify(title)
+        self.assertEqual(test_slug, self.obj_a.slug)
     def test_validate_title(self):
         qs = Video.objects.filter(title="This is my first title")
         self.assertEqual(qs.exists(), 1)
 
     def test_video_count(self):
         qs = Video.objects.all()
-        self.assertEqual(qs.count(), 1)
-
+        self.assertEqual(qs.count(), 2)
 
     def test_draft_case(self):
         qs = Video.objects.filter(state=Video.VideoStateOptions.DRAFT)
         self.assertEqual(qs.count(),1)
+
+    def test_publish_case(self):
+        now = timezone.now()
+        qs = Video.objects.filter(state=Video.VideoStateOptions.PUBLISH,published_timestamp__lte=now)
+        self.assertTrue(qs.exists())
+
+    
